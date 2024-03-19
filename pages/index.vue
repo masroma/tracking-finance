158450fe0ae337e32a932b75aec83cbf99d5c662
unknown <mohamad.romadhon@paramadina.ac.id>
2024-03-19 14:50:03 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl font-extrabold">Summary</h1>
+
+        <div>
+            <UInputMenu v-model="selectedView" :options="transactionViewOptions" />
+        </div>
+    </section>
+
+    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+        <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+        <Trend color="red" title="Expanse" :amount="4000" :last-amount="6000" :loading="false"/>
+        <Trend color="green" title="Investmen" :amount="6000" :last-amount="3000" :loading="false"/>
+        <Trend color="red" title="Saving" :amount="4000" :last-amount="7000" :loading="false"/>
+    </section>
+
+    <section>
+        <Transaction />
+        <Transaction />
+        <Transaction />
+        <Transaction />
+    </section>
+</template>
+
+<script setup>
+import { transactionViewOptions } from '~/constants';
+const selectedView = ref(transactionViewOptions[1])
+</script>
